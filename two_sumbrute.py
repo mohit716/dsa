eea4ac1b1c4caba9a38d2c0270afc34396f7b279
Mohit <200160784+mohit716@users.noreply.github.com>
2025-09-06 22:14:29 -0400

    feat: add matching parenthesis v1

--- two_sumbrute.py
+++ two_sumbrute.py
@@ -1,51 +1,89 @@
-'''
-two sum
-Question 1 (Easy – Hash Map / Arrays)
+# given a string containing just the characters
 
-👉 Problem:
-You are given an array of integers nums and an integer target. Return the indices of the two numbers that add up to target.
+# (    )     {       }        [       ]
 
-Assume exactly one solution exists.
+# determine  if the input string is valid
 
-Do not use the same element twice.
-
-Example:
-
-Input: nums = [1, 3, 4, 2], target = 6
-Output: [1, 2]   # because nums[1] + nums[2] = 3 + 4 = 7
-
-'''
+# A string is valid if :
 
 
+# Open brackets are closed by the same type of brackets 
 
-'''
-solution)
-1, 3, 4, 2 
-1 and target -1 which is 5 
-if 5 in dictionary's keys   
-i.e dictinary.get(i,0) means if i has an entry in the dictionary, return its value; otherwise, return 0.
-wait:
-it was d.get(i,0)+1  means if i is already in the dictionary, increment its count by 1; otherwise, initialize its count to 1.
-wait
-its needed to check if target-i is in the dictionary
-
-wait the if also not needed
-only .get(i,0) is enough
+# Open brackets are closed by the correct order
 
 
-'''
+# like: (  {  [  ]  }  )    output: true 
 
-def two_sum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-# Time complexity: O(n^2)
-# Space complexity: O(1)
-def main():
-    nums = [1, 3, 4, 2]
-    target = 6
-    result = two_sum(nums, target)
-    print(result)  
+# like: (  {  )  ]    output: false 
+
+
+# intuition: checking open close           
+# we scan one by one
+# if its a open bracket stack.append(ch)
+# if its not a openning bracket then the stack must have something , we will check if it has someting
+# what it means if id doesnt have anything ? it means we have a closing bracket(in about to enter thing) 
+# without an opening bracket return false
+# now the under case within the else is that the stack has something
+# we will pop it and check if it matches the closing bracket(in about to enter thing)
+# important we used dictionary to map the closing and opening brackets
+
+def isValid(s: str) -> bool:
+    stack = []
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    for ch in s:
+        if ch in mapping.values():
+            stack.append(ch)
+        else:
+            if not stack:
+                return False
+            top_element = stack.pop()
+            if mapping[ch] != top_element:
+                return False
+
+    return not stack
+
+# so main line is mapping[ch] != top_element:
+# map was used to map the closing and opening brackets
+# map[ch] gives corresponding opening bracket for the closing bracket ch
+# if it is not equal to the top element of the stack return false
+# so thing to remember is make dictionary to map closing and opening brackets
+# and use stack to keep track of opening brackets
+# and when you encounter closing bracket check if stack is not empty
+# if it is empty return false
+# if it is not empty pop the top element and check if it matches the closing bracket(through map)
+# if it does not match return false
+# one more thing order of mapping is important i.e first round then curly then square
+# also in loop we check if ch in mapping.values() i.e if it is opening bracket
+# if it is opening bracket we append it to stack
+# again we checked if its opening bracket through mapping.values()
+# if it is not opening bracket then it must be closing bracket
+# closing + stack empty = false 
+# why? because closing bracket without opening bracket is invalid
+# a case like this )(
+# anoter case like this (])
+# if we dry run this code on (])
+# first ( is opening bracket so stack = ['(']
+# then ] is closing bracket so stack is not empty we pop the top element which is (
+# now we check if mapping[']'] != '('
+# mapping[']'] is '[' so it is not equal to '(' so return false
+
+# another dry run on ( { [ ] } )
+# first ( is opening bracket so stack = ['(']
+# then { is opening bracket so stack = ['(', '{']
+# then [ is opening bracket so stack = ['(', '{', '[']
+# then ] is closing bracket so stack is not empty we pop the top element which is [
+# now we check if mapping[']'] != '['
+# mapping[']'] is '[' so it is equal to '[' so continue
+# now stack = ['(', '{']
+# then } is closing bracket so stack is not empty we pop the top element which is {
+# now we check if mapping['}'] != '{'
+# mapping['}'] is '{' so it is equal to '{' so continue
+# now stack = ['(']
+# then ) is closing bracket so stack is not empty we pop the top element which is ( 
+# now we check if mapping[')'] != '('
+# mapping[')'] is '(' so it is equal to '(' so continue
+# now stack = []
+# at the end we return not stack which is True because stack is empty
 
         
